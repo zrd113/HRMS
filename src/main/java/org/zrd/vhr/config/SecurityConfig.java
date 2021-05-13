8147919e7aba196f333233f83baef537f4665a56
zrd113 <820546971@qq.com>
2021-05-13 16:08:16 +0800

    优化验证码登录

--- src/main/java/org/zrd/vhr/config/SecurityConfig.java
+++ src/main/java/org/zrd/vhr/config/SecurityConfig.java
@@ -18,7 +18,6 @@ import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.access.intercept.FilterSecurityInterceptor;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
 import org.zrd.vhr.bean.Hr;
 import org.zrd.vhr.bean.RespBean;
@@ -45,8 +44,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     CustomUrlDecisionManager customUrlDecisionManager;
 
-    @Autowired
-    VerificationCodeFilter verificationCodeFilter;
+    //@Autowired
+    //VerificationCodeFilter verificationCodeFilter;
 
     @Bean
     PasswordEncoder passwordEncoder() {
@@ -63,9 +62,17 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         web.ignoring().antMatchers("/login", "/verifyCode");
     }
 
+    @Bean
+    MyAuthenticationProvider myAuthenticationProvider() {
+        MyAuthenticationProvider myAuthenticationProvider = new MyAuthenticationProvider();
+        myAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        myAuthenticationProvider.setUserDetailsService(userDetailsService());
+        return myAuthenticationProvider;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.addFilterBefore(verificationCodeFilter, UsernamePasswordAuthenticationFilter.class);
+        //http.addFilterBefore(verificationCodeFilter, UsernamePasswordAuthenticationFilter.class);
         http.authorizeRequests()
                 .anyRequest().authenticated()
                 .withObjectPostProcessor(new ObjectPostProcessor<FilterSecurityInterceptor>() {
